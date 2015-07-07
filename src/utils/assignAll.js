a014d18573bc0d381a2e18b054e51d0fffdc8518
Steven Sojka <steelsojka@gmail.com>
2015-07-07 12:01:35 -0500

    fix(assignAll): fallback to regular assignment

--- src/utils/assignAll.js
+++ src/utils/assignAll.js
@@ -1,7 +1,12 @@
 'use strict';
 
 import without from 'lodash/array/without';
+import isObject from 'lodash/lang/isObject';
 
+/**
+ * Assigns all properties, including non-enumerable properties over
+ * to the destination object.
+ */
 export default function assignAll(to, from, excludes = []) {
   const names = without(Object.getOwnPropertyNames(from), ...excludes);
 
@@ -12,7 +17,13 @@ export default function assignAll(to, from, excludes = []) {
 
     if (!destDtor || destDtor.configurable) {
       let srcDtor = Object.getOwnPropertyDescriptor(from, name);
-      Object.defineProperty(to, name, srcDtor);
+
+      if (isObject(srcDtor)) {
+        Object.defineProperty(to, name, srcDtor);
+      } else {
+        // Fallback to regular assignment
+        to[name] = from[name];
+      }
     }
   }
 
