bc6961cbfda0a8f34e2d54a525806d6f34dad3ff
steelsojka <steelsojka@gmail.com>
2015-09-27 15:30:39 -0500

    1.0.3

--- gulp/tasks/stage.js
+++ gulp/tasks/stage.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import config from '../config';
 import del from 'del';
 
-gulp.task('stage', ['clean:stage'], () => {
+gulp.task('stage', ['clean:stage', 'build'], () => {
   gulp.src(config.paths.published)
     .pipe(gulp.dest(config.paths.stage));
 });
