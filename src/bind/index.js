bc50f17654414535e6ad3074707298d1675f0e3f
Steven Sojka <steelsojka@gmail.com>
2016-01-07 07:09:57 -0600

    add bind util methods to export

--- src/bind/index.js
+++ src/bind/index.js
@@ -1,7 +1,11 @@
 import bind from './bind';
 import bindAll from './bindAll';
+import bindAllClassMethods from './bindAllClassMethods';
+import bindMap from './bindMap';
 
 export { bind };
 export { bindAll };
 export { bindAll as BindAll };
 export { bind as Bind };
+export { bindAllClassMethods as bindAllMethods };
+export { bindMap };
