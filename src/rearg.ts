65f8757f1e85bf8a1e16e83bc517b415a47efd3c
Steven Sojka <steelsojka@gmail.com>
2017-05-06 08:08:25 -0500

    fix(rearg): fix typescript build error

--- src/rearg.ts
+++ src/rearg.ts
@@ -8,7 +8,7 @@ import {
 } from './factory';
 import { PartialValueApplicator } from './applicators';
 
-export const Rearg: (indexes: ResolvableFunction|number|number[], ...args: (number|number[])[]) => LodashDecorator = DecoratorFactory.createInstanceDecorator(
+export const Rearg: (indexes: ResolvableFunction|number|number[], ...args: Array<number|number[]>) => LodashDecorator = DecoratorFactory.createInstanceDecorator(
   new DecoratorConfig(rearg, new PartialValueApplicator(), { property: true })
 );
 export { Rearg as rearg };
