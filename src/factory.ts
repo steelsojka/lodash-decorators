fda1c7367d27610827fbbd512aa8032299bcd914
Steven Sojka <Steven.Sojka@tdameritrade.com>
2017-08-07 08:04:07 -0500

    fix(factory): use barrel files instead of node module resolution

---
+++ src/factory.ts
@@ -0,0 +1,3 @@
+export * from './factory/DecoratorConfig';
+export * from './factory/DecoratorFactory';
+export * from './factory/common';
--- src/factory/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './DecoratorConfig';
-export * from './DecoratorFactory';
-export * from './common';
